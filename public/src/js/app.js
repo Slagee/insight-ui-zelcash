481e061e7292b09e7eced1e887f694479168543e
unknown <kdo.jiny.nez.ja@gmail.com>
2018-07-18 16:19:25 +0200

    start

---
+++ public/src/js/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var testnet = false;
+var netSymbol = testnet ? 'TEL' : 'ZEL';
+
+var defaultLanguage = localStorage.getItem('insight-language') || 'en';
+var defaultCurrency = localStorage.getItem('insight-currency') || netSymbol;
+
+angular.module('insight',[
+  'ngAnimate',
+  'ngResource',
+  'ngRoute',
+  'ngProgress',
+  'ui.bootstrap',
+  'ui.route',
+  'monospaced.qrcode',
+  'gettext',
+  'angularMoment',
+  'insight.system',
+  'insight.socket',
+  'insight.blocks',
+  'insight.statistics',
+  'insight.richList',
+  'insight.transactions',
+  'insight.address',
+  'insight.search',
+  'insight.charts',
+  'insight.statistics',
+  'insight.status',
+  'insight.connection',
+  'insight.currency',
+  'insight.messages'
+]);
+
+angular.module('insight.system', []);
+angular.module('insight.socket', []);
+angular.module('insight.blocks', []);
+angular.module('insight.transactions', []);
+angular.module('insight.address', []);
+angular.module('insight.search', []);
+angular.module('insight.charts', []);
+angular.module('insight.richList', []);
+angular.module('insight.statistics', [ 'ngNumeraljs' ]);
+angular.module('insight.status', []);
+angular.module('insight.connection', []);
+angular.module('insight.currency', []);
+angular.module('insight.messages', []);
